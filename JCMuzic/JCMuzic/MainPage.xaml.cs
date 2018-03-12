16886b65d2bb838defc6f789f502d9df15af77d0
Kokul Jose <kokuljose@outlook.com>
2018-03-12 23:14:43 +0530

    Play local file explicitly.

--- JCMuzic/JCMuzic/MainPage.xaml.cs
+++ JCMuzic/JCMuzic/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.MediaManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,20 @@ namespace JCMuzic
 		public MainPage()
 		{
 			InitializeComponent();
+
 		}
-	}
+
+        private async void PlayPauseClicked(object sender, EventArgs e)
+        {
+
+            //await CrossMediaManager.Current.Play("https://raw.githubusercontent.com/martijn00/XamarinMediaManager/develop/Samples/Simple/MediaSample.Core/Media/local-sample.mp3");
+
+            await CrossMediaManager.Current.Play("/sdcard/11.mp3");
+        }
+
+        private async void StopButton_OnClicked(object sender, EventArgs e)
+        {
+            await CrossMediaManager.Current.Stop();
+        }
+    }
 }
